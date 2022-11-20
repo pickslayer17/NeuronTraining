70a02ea2233a94d2ba3a51ba55c32de3920a9edc
100nout.by <Dzianis.piatrovich@gmail.com>
2022-11-20 17:28:40 +0200

    Move static method from Matrix to MatrixCalculator

---
+++ NeuronTraining/MatrixCalculator.cs
@@ -0,0 +1,106 @@
+﻿namespace NeuronTraining
+{
+    public static class MatrixCalculator
+    {
+        public const double Exp = 2.71828;
+
+        public static Matrix Square(Matrix matrixToSquare)
+        {
+            var result = new Matrix(matrixToSquare.RowCount, matrixToSquare.ColumnCount);
+
+            for (int i = 0; i < matrixToSquare.RowCount; i++)
+            {
+                for (int j = 0; j < matrixToSquare.ColumnCount; j++)
+                {
+                    result[i, j] = Math.Pow(matrixToSquare[i, j], 2);
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix Subtract(Matrix minuend, Matrix subtrahend)
+        {
+            var result = new Matrix(minuend.RowCount, minuend.ColumnCount);
+
+            for (int i = 0; i < minuend.RowCount; i++)
+            {
+                for (int j = 0; j < minuend.ColumnCount; j++)
+                {
+                    var diff = minuend[i, j] - subtrahend[i, j];
+                    result[i, j] = diff;
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix Transponse(Matrix matrixToTransponse)
+        {
+            var result = new Matrix(matrixToTransponse.ColumnCount, matrixToTransponse.RowCount);
+
+            for (int i = 0; i < matrixToTransponse.RowCount; i++)
+            {
+                for (int j = 0; j < matrixToTransponse.ColumnCount; j++)
+                {
+                    result[j, i] = matrixToTransponse[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix ApplySigmoid(Matrix inputMatrix)
+        {
+            var result = new Matrix(inputMatrix.RowCount, inputMatrix.ColumnCount);
+
+            for (int i = 0; i < inputMatrix.RowCount; i++)
+            {
+                var sigmoidRes = Sigmoid(inputMatrix[i, 0]);
+                result[i, 0] = sigmoidRes;
+            }
+
+            return result;
+        }
+
+        public static Matrix GetMatrixFromArray(double[,] array)
+        {
+            var result = new Matrix(array.GetLength(0), array.GetLength(1));
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    result[i, j] = (double)array.GetValue(i, j);
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix MultiplyMatrix(Matrix weightsMatrix, Matrix inputMatrix)
+        {
+            var result = new Matrix(inputMatrix.RowCount, inputMatrix.ColumnCount);
+
+            for (int i = 0; i < weightsMatrix.RowCount; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < weightsMatrix.ColumnCount; j++)
+                {
+                    sum += inputMatrix[j, 0] * weightsMatrix[i, j];
+                }
+
+                result[i, 0] = sum;
+            }
+
+            return result;
+        }
+
+        public static double Sigmoid(double x)
+        {
+            var divider = 1 + Math.Pow(Exp, -x);
+            var result = 1 / divider;
+
+            return Math.Round(result, 10);
+        }
+    }
+}
--- NeuronTraining/Program.cs
+++ NeuronTraining/Program.cs
@@ -1,51 +1,49 @@
-﻿using System.Transactions;
-
-namespace NeuronTraining
+﻿namespace NeuronTraining
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var I = Matrix.GetMatrixFromArray(new double[,]
+            var I = MatrixCalculator.GetMatrixFromArray(new double[,]
             {
                 { 0.9 }, // input on 1st
                 { 0.1 }, // input on 2nd
                 { 0.8 }  // input on 3rd
             });
 
-            var W = Matrix.GetMatrixFromArray(new double[,]
+            var W = MatrixCalculator.GetMatrixFromArray(new double[,]
             {
                 { 0.9, 0.3, 0.4 }, // weights to 1st neuron
                 { 0.2, 0.8, 0.2 }, // weights to 2nd neuron
                 { 0.1, 0.5, 0.6 }  // weights to 3rd neuron
             });
 
-            var X = Matrix.MultiplyMatrix(W, I);
-            var O = Matrix.ApplySigmoid(X);
+            var X = MatrixCalculator.MultiplyMatrix(W, I);
+            var O = MatrixCalculator.ApplySigmoid(X);
 
-            var W2 = Matrix.GetMatrixFromArray(new double[,]
+            var W2 = MatrixCalculator.GetMatrixFromArray(new double[,]
             {
                 { 0.3, 0.7, 0.5 },
                 { 0.6, 0.5, 0.2 },
                 { 0.8, 0.1, 0.9 }
             });
 
-            var X2 = Matrix.MultiplyMatrix(W2, O);
-            var O2 = Matrix.ApplySigmoid(X2);
+            var X2 = MatrixCalculator.MultiplyMatrix(W2, O);
+            var O2 = MatrixCalculator.ApplySigmoid(X2);
 
             //--------------------------
             // Final etalon output
-            var T = Matrix.GetMatrixFromArray(new double[,]
+            var T = MatrixCalculator.GetMatrixFromArray(new double[,]
             {
                 { 1 },
                 { 0 },
                 { 0 }
             });
-            
-            var Efinal = Matrix.Subtract(T, O2);
-            Efinal = Matrix.Square(Efinal);
-            var W2t = Matrix.Transponse(W2);
-            var E2 = Matrix.MultiplyMatrix(W2t, Efinal);
+
+            var Efinal = MatrixCalculator.Subtract(T, O2);
+            Efinal = MatrixCalculator.Square(Efinal);
+            var W2t = MatrixCalculator.Transponse(W2);
+            var E2 = MatrixCalculator.MultiplyMatrix(W2t, Efinal);
 
             Console.WriteLine("first output");
             MatrixConsoleWriter.OutMatrix(X);
